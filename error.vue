9496d12533f8c5fd9d68ed2d9dd9238f5e628145
Laurenzooo <laurenz.blanck@gmail.com>
2026-03-02 22:23:44 +0100

    small fix

--- error.vue
+++ error.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-definePageMeta({
-	layout: 'default',
-});
-
 const props = defineProps({
 	error: Object,
 });
@@ -11,7 +7,11 @@ const handleError = () => clearError({ redirect: '/' });
 </script>
 
 <template>
-	<div>
-		{{ error }}
-	</div>
+	<NuxtLayout name="default">
+		<div class="flex flex-col items-center justify-center min-h-[50vh] text-center p-8">
+			<h1 class="text-4xl font-bold mb-4">Oops! Something went wrong.</h1>
+			<p class="text-lg text-gray-600 mb-8">{{ error?.message || 'An unexpected error occurred.' }}</p>
+			<UButton color="black" @click="handleError">Return Home</UButton>
+		</div>
+	</NuxtLayout>
 </template>
